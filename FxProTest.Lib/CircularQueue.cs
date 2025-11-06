08b55677f558c5cb974f5ae3445f4f734d743c74
Hamlet Hakobyan <hamlet.hakobyan@outlook.com>
2025-11-07 00:43:34 +0400

    Initial code

---
+++ FxProTest.Lib/CircularQueue.cs
@@ -0,0 +1,68 @@
+﻿namespace FxProTest.Lib;
+
+public class CircularQueue<T>
+{
+    private readonly T[] _storage;
+    // read point can be [0..n-1]
+    private int _read;
+    // write point can be [0..2n -1]
+    // so that e can handle whole buffer (n-1 - 2n-1 == n)
+    private int _write;
+
+    public CircularQueue(int capacity)
+    {
+        _storage = new T[capacity];
+        _read = 0;
+        _write = 0;
+    }
+
+    public void Enqueue(T item)
+    {
+        _storage[_write % _storage.Length] = item;
+        IncrementWritePointer();
+    }
+
+    private void IncrementWritePointer()
+    {
+        // move write to next slot
+        _write++;
+        // check if we make a cycle on write and reach to read pointer (buffer full)
+        if (_write > _read + _storage.Length)
+        {
+            // move read pointer further most old slot
+            _read++;
+        }
+
+        if (_read == _storage.Length)
+        {
+            _read = 0;
+        }
+
+        // when write pointer have made 
+        if (_write == 2 * _storage.Length)
+        {
+            _write = _storage.Length;
+        }
+    }
+
+    private void IncrementReadPointer()
+    {
+        _read++;
+        if (_read == _storage.Length)
+        {
+            _read = 0;
+            _write -= _storage.Length;
+        }
+    }
+
+    public T Dequeue()
+    {
+        if(Count == 0) throw new InvalidOperationException();
+        var res = _storage[_read % _storage.Length];
+        IncrementReadPointer();
+        
+        return res;
+    }
+    
+    public int Count => _write - _read;
+}
---
+++ FxProTest.Tests/CircularQueue.Tests.cs
@@ -0,0 +1,103 @@
+﻿namespace FxProTest.Tests;
+
+using FxProTest.Lib; 
+
+public class CircularQueueTests
+{
+    [Fact]
+    public void CircularQueue_Can_Create()
+    {
+        var circularQueue = new CircularQueue<int>(10);
+    }
+
+    [Fact]
+    public void CircularQueue_Should_Throw_When_Empty()
+    {
+        var circularQueue = new CircularQueue<int>(10);
+        Assert.Throws<InvalidOperationException>(() => circularQueue.Dequeue());
+    }
+
+    [Fact]
+    public void CircularQueue_Count_Should_Be_1_When_Enqueued()
+    {
+        var circularQueue = new CircularQueue<int>(10);
+        circularQueue.Enqueue(1);
+        Assert.Equal(1, circularQueue.Count);
+    }
+
+    [Fact]
+    public void CircularQueue_Deque_Should_Return_The_Item_When_Enqueued()
+    {
+        var circularQueue = new CircularQueue<int>(10);
+        circularQueue.Enqueue(1);
+        var actual = circularQueue.Dequeue();
+        Assert.Equal(1, actual);
+    }
+    
+    [Fact]
+    public void CircularQueue_Is_FIFO()
+    {
+        var circularQueue = new CircularQueue<int>(10);
+        circularQueue.Enqueue(1);
+        circularQueue.Enqueue(2);
+        var actual = circularQueue.Dequeue();
+        Assert.Equal(1, actual);
+    }
+
+    [Fact]
+    public void CircularQueue_Is_Circular_FIFO()
+    {
+        var circularQueue = new CircularQueue<int>(2);
+        circularQueue.Enqueue(0);
+        circularQueue.Enqueue(1);
+        circularQueue.Enqueue(2);
+        var actual = circularQueue.Dequeue();
+        
+        Assert.Equal(1, actual);
+        
+    }
+    
+    [Fact]
+    public void CircularQueue_Is_Circular_FIFO_In_More_Than_One_Cycle()
+    {
+        var circularQueue = new CircularQueue<int>(2);
+        circularQueue.Enqueue(0);
+        circularQueue.Enqueue(1);
+        circularQueue.Enqueue(2);
+        circularQueue.Enqueue(3);
+        circularQueue.Enqueue(4);
+
+        var actual = circularQueue.Dequeue();
+        
+        Assert.Equal(3, actual);
+    }
+    
+    [Theory]
+    [InlineData(3, 13)]
+    [InlineData(3, 14)]
+    [InlineData(3, 15)]
+    [InlineData(3, 16)]
+    [InlineData(3, 17)]
+    [InlineData(3, 2)]
+    [InlineData(8, 3)]
+    [InlineData(0, 0)]
+    [InlineData(1, 0)]
+    public void CircularQueue_Circular_FIFO_Should_Dequeue_All_In_Right_Order(int capacity, int n)
+    {
+        var circularQueue = new CircularQueue<int>(capacity);
+        for (var i = 0; i < n; ++i)
+        {
+            circularQueue.Enqueue(i);
+        }
+
+        var arraySize = Math.Min(capacity, n);
+        var actual = new int[arraySize];
+        for (var i = 0; i < actual.Length; i++)
+        {
+            actual[i] = circularQueue.Dequeue();
+        }
+        
+        Assert.Equal(Enumerable.Range(n < capacity ? 0 : (n - capacity), arraySize), actual);
+        Assert.Equal(0, circularQueue.Count);
+    }
+}
